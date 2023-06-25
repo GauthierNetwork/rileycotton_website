41e615e423176a763933aec8e97c04f8b6769168
GauthierNetwork <chris@gauthier.network>
2023-06-25 16:04:16 -0500

    updated pages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import { Page } from '../components/layout/Page';
 const Home = () => {
   return (
     <Page title={'Home'}>
-      <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-pink-500 to-purple-500 text-white">
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-pink-500 to-orange-500 text-white">
         <div className="p-8 rounded-lg shadow-lg bg-pink-400 text-center">
           <h1 className="text-6xl mb-8">Welcome to Riley's Portfolio</h1>
           <p className="mb-8 text-lg">
@@ -12,27 +12,27 @@ const Home = () => {
           </p>
         </div>
 
-        <div className="p-8 mt-8 rounded-lg shadow-lg bg-purple-400 text-center">
+        <div className="p-8 mt-8 rounded-lg shadow-lg bg-orange-400 text-center">
           <h2 className="text-4xl mb-8">My Work</h2>
           <p className="mb-8 text-lg">
             Explore my projects and experience in the portfolio
           </p>
           <Link
-            to="/portfolio"
-            className="px-4 py-2 bg-white text-purple-500 rounded hover:bg-purple-300 transition-colors"
+            to="/shop"
+            className="px-4 py-2 bg-white text-orange-500 rounded hover:bg-orange-300 transition-colors"
           >
             Go to Portfolio
           </Link>
         </div>
 
-        <div className="p-8 mt-8 rounded-lg shadow-lg bg-purple-400 text-center">
+        <div className="p-8 mt-8 rounded-lg shadow-lg bg-orange-400 text-center">
           <h2 className="text-4xl mb-8">Contact Me</h2>
           <p className="mb-8 text-lg">
             Get in touch for collaborations or any inquiries
           </p>
           <Link
             to="/contact"
-            className="px-4 py-2 bg-white text-purple-500 rounded hover:bg-purple-300 transition-colors"
+            className="px-4 py-2 bg-white text-orange-500 rounded hover:bg-orange-300 transition-colors"
           >
             Contact
           </Link>
