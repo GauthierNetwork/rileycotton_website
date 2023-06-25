86e023f1b934c922b4df8433b23a1b62bd1b9535
GauthierNetwork <chris@gauthier.network>
2023-06-25 16:04:09 -0500

    fixed tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Shop from './pages/Shop';
 import Home from './pages/Home';
+import 'tailwindcss/tailwind.css';
 
 function App() {
   return (
